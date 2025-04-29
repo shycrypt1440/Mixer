9568ab8acf6b637b7c1be43acfaa4dcf42f72310
shycrypt1440 <209580518+shycrypt1440@users.noreply.github.com>
2025-04-28 21:20:02 -0700

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const isProd = process.env.BUILD === 'production';
+const tsconfig = isProd ? './tsconfig.prod.json' : './tsconfig.json'
+
+export default {
+  input: 'src/main.ts',
+  treeshake: false,
+  output: {
+    dir: 'dist/Mixer',
+    sourcemap: isProd ? false : 'inline',
+    sourcemapExcludeSources: isProd,
+    format: 'cjs',
+    exports: 'default',
+  },
+  plugins: [
+    typescript({ tsconfig }),
+    nodeResolve(),
+    commonjs(),
+    isProd && terser({
+          format: {
+            comments: false
+          },
+          sourceMap: false,
+          compress: true
+        })
+  ],
+};
